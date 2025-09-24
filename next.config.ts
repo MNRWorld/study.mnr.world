0e6e9551c88d83e05ce6927c9234d4be6a2596f7
FrostFoe <frostfoe@gmail.com>
2025-09-24 13:16:44 +0000

    add redirect to /du to university/dhaka

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/du',
+        destination: '/university/dhaka',
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
