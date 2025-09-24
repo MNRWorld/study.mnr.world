c04d2956f1500766dac124e1c6d8e2083669d387
FrostFoe <frostfoe@gmail.com>
2025-09-24 03:55:56 +0000

    add in in main webapp

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import { FloatingNavBar } from '@/components/ui/floating-navbar';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +11,9 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
         {children}
       </main>
       <Footer />
+      <div className="fixed bottom-10 left-1/2 -translate-x-1/2 z-50">
+        <FloatingNavBar />
+      </div>
     </div>
   );
 }
