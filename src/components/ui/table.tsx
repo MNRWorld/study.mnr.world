ffaaca0cb068b1755980700baae5b28f69033b1c
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-09-25 17:24:31 +0000

    src/components/ui/table.tsx: এই ফাইলে TableBody কম্পোনেন্টে [&_tr:nth-ch

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -30,7 +30,7 @@ const TableBody = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <tbody
     ref={ref}
-    className={cn("[&_tr:last-child]:border-0 [&_tr:nth-child(even)]:bg-muted/50", className)}
+    className={cn("[&_tr:last-child]:border-0 [&_tr:nth-child(odd)]:bg-muted/50", className)}
     {...props}
   />
 ))
@@ -87,7 +87,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ))
