c5a17fae2fb03ce089d2f3f2d6a7251728d01279
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-09-25 14:45:38 +0000

    আর একটা ব্যাপার /du দিলে আসে। কিন্তু, /DU দিলে আসেনা। এইটাও ফিক্স করা লা

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/DU',
+        destination: '/du',
+        permanent: true,
+      },
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
