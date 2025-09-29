fd0888573e16395f86f0910f7d6b13b1ff3e02a6
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-09-29 15:37:56 +0000

    সব

--- src/lib/data/admission-schedule.ts
+++
@@ -1,9 +0,0 @@
-import scheduleData from "./admission-schedule.json";
-
-export interface AdmissionScheduleItem {
-  subject: string;
-  date: string;
-  status: string;
-}
-
-export const admissionSchedule: AdmissionScheduleItem[] = scheduleData;
