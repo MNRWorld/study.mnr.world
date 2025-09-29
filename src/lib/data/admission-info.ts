0522ec90aa7345242ed5a5908c0cef599b4242c1
FrostFoe <frostfoe@gmail.com>
2025-09-29 07:28:32 +0000

    Is it possible to integrate multiple web pages, including the admission

--- src/lib/data/admission-info.ts
+++ src/lib/data/admission-info.ts
@@ -3,4 +3,4 @@ import admissionInfo from "./admission-info.json";
 export const duAdmissionInfo = admissionInfo.du;
 export const duHistoryAndMapData = admissionInfo.duHistoryAndMap;
 export const collegeAdmissionInfo = admissionInfo.college;
-export const privateAdmissionInfo = admissionInfo.private;
+export const privateAdmissionInfo = admission-info.private;
