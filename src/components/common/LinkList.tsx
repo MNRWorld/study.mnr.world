fe84fe0d08f759337303b10e477ac798420dedf3
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-09-30 13:26:33 +0000

    এই প্যডিং টা রিমুভ করে দেন

--- src/components/common/LinkList.tsx
+++ src/components/common/LinkList.tsx
@@ -41,7 +41,7 @@ const LinkList = ({ links }: LinkListProps) => {
                     href={link.href}
                     target={link.target}
                     rel={link.rel}
-                    className="block w-full hover:bg-accent rounded-md py-2 px-2"
+                    className="block w-full hover:bg-accent rounded-md py-2"
                   >
                     <div
                       className="text-base"
