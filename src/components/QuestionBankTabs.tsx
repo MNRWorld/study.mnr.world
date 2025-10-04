ec2b28d16500d8ac12354c97bc7c6cf9314ed498
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-10-04 17:25:49 +0000

    ব্যাকগ্রাউন্ড কালার এভাবে পরিবর্তন করিয়েন:
        primary কালার: এটি আপনার

--- src/components/QuestionBankTabs.tsx
+++ src/components/QuestionBankTabs.tsx
@@ -22,7 +22,7 @@ const QuestionBankTabs = () => {
       className="mt-8 w-full border border-border bg-card rounded-2xl p-4 sm:p-6 shadow-lg text-center relative"
     >
       <div className="flex justify-center">
-        <div className="inline-block px-6 py-2 bg-gradient-to-r from-primary to-foreground text-primary-foreground rounded-full text-lg mb-4 font-bold shadow-md">
+        <div className="gradient-background inline-block px-6 py-2 text-primary-foreground rounded-full text-lg mb-4 font-bold shadow-md">
           প্রশ্নব্যাংক
         </div>
       </div>
