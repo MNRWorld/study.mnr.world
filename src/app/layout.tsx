c10db5778bd7c5a1ecee0d5de156075e77dab293
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-10-06 11:41:02 +0000

    pt-10 এর পরিবর্তে pt-5 করে দিন

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
         >
           <SupabaseProvider>
             <Header />
-            <main className="flex-grow pt-10">{children}</main>
+            <main className="flex-grow pt-5">{children}</main>
             <Footer />
             <BackToTopButton />
             <Toaster />
