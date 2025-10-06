eb24f9dd76c472587c47dcab58e23a1aaedd5a21
FrostFoe <frostfoe@gmail.com>
2025-10-06 06:24:06 +0000

    use these
      apiKey: "AIzaSyBX4Q3fL1dX58M2NZ1xIA7OwheeubtGCuw",
      authDo

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,10 +8,10 @@ import { FirebaseOptions } from "firebase/app";
 // Firebase console and add it to your environment variables.
 // See: https://firebase.google.com/docs/web/setup
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBX4Q3fL1dX58M2NZ1xIA7OwheeubtGCuw",
+  authDomain: "studio-4449027191-94248.firebaseapp.com",
+  projectId: "studio-4449027191-94248",
+  storageBucket: "studio-4449027191-94248.appspot.com",
+  messagingSenderId: "733159583803",
+  appId: "1:733159583803:web:e0e0dff1c9862900e6ac1e"
 };
