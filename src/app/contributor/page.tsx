1dca3538dc8111beb880147f9bb2047116380ec8
FrostFoe <frostfoe@gmail.com>
2025-10-06 14:58:19 +0000

    /contributor page ta ekdom khali blank kore den ar gasp package ar impor

--- src/app/contributor/page.tsx
+++ src/app/contributor/page.tsx
@@ -1,16 +1,7 @@
-import FlowingMenu from '@/components/FlowingMenu';
-
-const demoItems = [
-  { link: '#', text: 'FrostFoe', image: 'https://raw.githubusercontent.com/FrostFoe/recyclebin/refs/heads/main/images/logo.png' },
-  { link: '#', text: 'Naimur', image: 'https://raw.githubusercontent.com/MNRfrom2020/logo-and-icon-cdn/refs/heads/main/Logo/Naimur/Naimur%20BGB.png' },
-  { link: '#', text: 'Sadman', image: 'https://raw.githubusercontent.com/MNRfrom2020/logo-and-icon-cdn/refs/heads/main/Logo/Sadman/Sadman.jpg' },
-  { link: '#', text: 'Alif', image: 'https://raw.githubusercontent.com/MNRfrom2020/logo-and-icon-cdn/refs/heads/main/Logo/Alif/Alif.jpg' }
-];
-
 export default function ContributorPage() {
   return (
-    <div style={{ height: '600px', position: 'relative' }}>
-      <FlowingMenu items={demoItems} />
+    <div>
+      {/* This page is intentionally left blank. */}
     </div>
   );
 }
