2ec348166ca5f0a5cd822fb4f1cfc05259f2d1c5
FrostFoe <frostfoe@gmail.com>
2025-10-06 13:16:22 +0000

    Try fixing this error: `Runtime Error: Error: The default export is not

--- src/app/contributor/page.tsx
+++ src/app/contributor/page.tsx
@@ -1,8 +1,16 @@
+import FlowingMenu from '@/components/FlowingMenu';
+
+const demoItems = [
+  { link: '#', text: 'FrostFoe', image: 'https://raw.githubusercontent.com/FrostFoe/recyclebin/refs/heads/main/images/logo.png' },
+  { link: '#', text: 'Naimur', image: 'https://raw.githubusercontent.com/MNRfrom2020/logo-and-icon-cdn/refs/heads/main/Logo/Naimur/Naimur%20BGB.png' },
+  { link: '#', text: 'Sadman', image: 'https://raw.githubusercontent.com/MNRfrom2020/logo-and-icon-cdn/refs/heads/main/Logo/Sadman/Sadman.jpg' },
+  { link: '#', text: 'Alif', image: 'https://raw.githubusercontent.com/MNRfrom2020/logo-and-icon-cdn/refs/heads/main/Logo/Alif/Alif.jpg' }
+];
 
 export default function ContributorPage() {
   return (
-    <div>
-      {/* This page is intentionally left blank for now. */}
+    <div style={{ height: '600px', position: 'relative' }}>
+      <FlowingMenu items={demoItems} />
     </div>
   );
 }
