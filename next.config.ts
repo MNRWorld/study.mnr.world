f8235f5e422c4d70b15eb2755fdeace718ec9024
MNR from 2020 <contact.mnrfrom2020@gmail.com>
2025-10-13 05:26:35 +0000

    study.mnr.world theke ki ki image load hocche?

--- next.config.ts
+++ next.config.ts
@@ -9,12 +9,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "study.mnr.world",
-        port: "",
-        pathname: "/**",
-      },
       { protocol: "https", hostname: "raw.githubusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
